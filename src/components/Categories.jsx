17e3335a4a3b6497b96e6510187664200ef15e66
PabloPicassoWhat <magnimunin@gmail.com>
2022-08-01 20:02:49 +0300

    adds the start structure and styles of the app

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import '../scss/app.scss';
+
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const category = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+  return (
+    <div className="categories">
+      <ul>
+        {category.map((catg, index) => (
+            <li key={index} onClick={() => setActiveIndex(index)} className={activeIndex === index ? "active" : ""}>{catg}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
