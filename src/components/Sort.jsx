59ee474bfd3703f6d0bdea9da2fd45d38f31cdcd
PabloPicassoWhat <magnimunin@gmail.com>
2022-08-04 22:03:01 +0300

    adds search and pagination

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
 
-const Sort = ({selectItem, setSelectItem, setSortPosition, sortPosition}) => {
+const Sort = ({selectItem, setSelectItem, setSortPosition, sortPosition, popupSort, setPopupSort}) => {
   const arrList = [
     {name: 'популярности', sortType: 'rating'},
     {name: 'цене', sortType: 'price'},
     {name: 'алфавиту', sortType: 'title'}
   ]
-  const [popupSort, setPopupSort] = useState(false)
 
   const onClickItem = (obj) => {
     setSelectItem(obj)
@@ -16,18 +15,10 @@ const Sort = ({selectItem, setSelectItem, setSortPosition, sortPosition}) => {
   return (
     <div className="sort">
       <div className="sort__label">
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
+        {!popupSort
+          ? <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fillRule="evenodd" d="M0 5l6 6 6-6H0z"/></svg>
+          : <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fillRule="evenodd" d="M12 11L6 5l-6 6h12z"/></svg>
+        }
         <b>Сортировка по:</b>
         <span onClick={() => setPopupSort(!popupSort)}>{selectItem.name}</span>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,37 +4,46 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaSkeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
+import Pagination from "../components/Pagination";
 
-const Home = () => {
+const Home = ({searchValue}) => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [categoryId, setActiveCategory] = useState(0)
   const [selectSortItem, setSelectSortItem] = useState({name: 'популярности', sortType: 'rating'})
   const [sortPosition, setSortPosition] = useState(false)
+  const [popupSort, setPopupSort] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
 
-  const category = !categoryId ? "" : categoryId
+  const category = !categoryId ? "" : `category=${categoryId}`
   const sortBy = selectSortItem.sortType
   const order = !sortPosition ? 'asc' : 'desc'
+  const search = searchValue ? `&search=${searchValue}` : ''
 
   useEffect(() => {
     setIsLoading(true)
-    fetch(`https://62e8ff67249bb1284eb82257.mockapi.io/items?category=${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(`https://62e8ff67249bb1284eb82257.mockapi.io/items?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`)
       .then(response => response.json())
       .then(arr => {
         setItems(arr)
         setIsLoading(false)
       })
     window.scrollTo(0, 0)
-  }, [categoryId, selectSortItem])
+  }, [categoryId, selectSortItem, searchValue, currentPage])
+
+  const skeleton = [...new Array(8)].map((_, i) => <PizzaSkeleton key={i}/>)
+  const pizzaElement = items.map((item) => <PizzaBlock key={item.id} {...item}/>)
 
   return (
-    <div className="container">
+    <div onPointerLeave={() => setPopupSort(false)} className="container">
       <div className="content__top">
         <Categories
           valueCategory={categoryId}
           onClickCategory={(i) => setActiveCategory(i)}
         />
         <Sort
+          popupSort={popupSort}
+          setPopupSort={setPopupSort}
           selectItem={selectSortItem}
           setSelectItem={setSelectSortItem}
           sortPosition={sortPosition}
@@ -44,9 +53,11 @@ const Home = () => {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-          ? [...new Array(10)].map((_, i) => <PizzaSkeleton key={i}/>)
-          : items.map((item) => <PizzaBlock key={item.id} {...item}/>)}
+          ? skeleton
+          : pizzaElement
+        }
       </div>
+      <Pagination setCurrentPage={setCurrentPage}/>
     </div>
   );
 };
