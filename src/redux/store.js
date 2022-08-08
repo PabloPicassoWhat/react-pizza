71dd093a6414a1fc74571d690a1276bc2a1c84cc
PabloPicassoWhat <magnimunin@gmail.com>
2022-08-08 23:48:27 +0300

    adds cart logic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterReducer from './slices/filterSlice'
 import paginationReducer from './slices/paginationSlice'
+import cartReducer from './slices/cartSlice'
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
     pagination: paginationReducer,
+    cart: cartReducer
   },
 })
